912e99ca2e34cde4a63564eb47fba044d285e734
Mikiyas Alemayehu <130913224+MikiAlexo@users.noreply.github.com>
2025-10-08 21:13:27 +0300

    Update TimeManager.cpp

--- Bello mini/src/TimeManager.cpp
+++ Bello mini/src/TimeManager.cpp
@@ -12,15 +12,8 @@ byte currentHour = hour();
 byte currentMinute = minute();
 
 String nextbell(String next_bell){
-// Define arrays to store period hours and minutes
-  //  int periodHours[] = {period_1_h, period_2_h, period_3_h, period_4_h, period_5_h, period_6_h, period_7_h, period_8_h, period_9_h, period_10_h};
-  //  int periodMinutes[] = {period_1_m, period_2_m, period_3_m, period_4_m, period_5_m, period_6_m, period_7_m, period_8_m, period_9_m, period_10_m};
-  // Get current hour and minute
 
-
-  // Iterate over periods
   for (byte i = 0; i < 10; i++) {
-    // Check if current period is active
     if ((period_h[i] < currentHour || (period_h[i] == currentHour && period_m[i] <= currentMinute)) &&
         (period_h[(i + 1) % 10] > currentHour || (period_h[(i + 1) % 10] == currentHour && period_m[(i + 1) % 10] > currentMinute))) {
       // Update next_bell
@@ -58,9 +51,6 @@ String ad_O(){
 }
 
 void bell() {
-  // Define arrays to store period hours and minutes
-  //  int periodHours[] = {period_1_h, period_2_h, period_3_h, period_4_h, period_5_h, period_6_h, period_7_h, period_8_h, period_9_h, period_10_h};
-  //  int periodMinutes[] = {period_1_m, period_2_m, period_3_m, period_4_m, period_5_m, period_6_m, period_7_m, period_8_m, period_9_m, period_10_m};
 
   // Iterate over periods
   for (byte i = 0; i < 10; i++) {
@@ -117,5 +107,6 @@ digitalWrite(Relay_pin,HIGH);
 Serial.println("manual_ringing");
 }
 digitalWrite(Relay_pin,LOW);
+
 
 }
