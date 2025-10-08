869a9fdf017d76f5a0eae4399afb0f3ee94d90e9
Mikiyas Alemayehu <130913224+MikiAlexo@users.noreply.github.com>
2025-10-08 23:26:16 +0300

    Update display.h

--- Bello mini/src/display.h
+++ Bello mini/src/display.h
@@ -5,12 +5,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
 
 
 #define OLED_RESET     -1 
-#define SCREEN_ADDRESS 0x3C// the curent oled display i have has this address but if this doesn't work try 0X3D
+#define SCREEN_ADDRESS 0x3C // the curent oled display i have has this address but if this doesn't work try 0X3D
 extern Adafruit_SSD1306 ddisplay;
+
 
 void mainMenu();
