b96e341bd1893bf700da64fff67b2cfec54cb65d
Mikiyas Alemayehu <130913224+MikiAlexo@users.noreply.github.com>
2025-10-08 21:14:25 +0300

    Update MemoryManager.h

--- Bello mini/src/MemoryManager.h
+++ Bello mini/src/MemoryManager.h
@@ -28,26 +28,7 @@ extern byte period_m[10];
  const byte  ADDR_PERIOD_10_H PROGMEM = 18;
  const byte  ADDR_PERIOD_10_M PROGMEM = 19;
 
-// extern const byte  ADDR_PERIOD_1_H;
-// extern const byte  ADDR_PERIOD_1_M;
-// extern const byte  ADDR_PERIOD_2_H; 
-// extern const byte  ADDR_PERIOD_2_M ;
-// extern const byte  ADDR_PERIOD_3_H ;
-// extern const byte  ADDR_PERIOD_3_M ;
-// extern const byte  ADDR_PERIOD_4_H ;
-// extern const byte  ADDR_PERIOD_4_M ;
-// extern const byte  ADDR_PERIOD_5_H ;
-// extern const byte  ADDR_PERIOD_5_M ;
-// extern const byte  ADDR_PERIOD_6_H ;
-//  const byte  ADDR_PERIOD_6_M PROGMEM = 11;
-//  const byte  ADDR_PERIOD_7_H PROGMEM = 12;
-//  const byte  ADDR_PERIOD_7_M PROGMEM = 13;
-//  const byte  ADDR_PERIOD_8_H PROGMEM = 14;
-//  const byte  ADDR_PERIOD_8_M PROGMEM = 15;
-//  const byte  ADDR_PERIOD_9_H PROGMEM = 16;
-//  const byte  ADDR_PERIOD_9_M PROGMEM = 17;
-//  const byte  ADDR_PERIOD_10_H PROGMEM = 18;
-//  const byte  ADDR_PERIOD_10_M PROGMEM = 19;
+
 
 
 
